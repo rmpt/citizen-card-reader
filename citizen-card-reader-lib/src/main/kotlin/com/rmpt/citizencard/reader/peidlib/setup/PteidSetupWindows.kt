777100eb42e0cb62111033a22a815e450be91a5b
Rui Teixeira <ruiteixeira.dev@gmail.com>
2024-03-25 17:04:21 +0000

    fixed issue with win 32/64 bits library

--- citizen-card-reader-lib/src/main/kotlin/com/rmpt/citizencard/reader/peidlib/setup/PteidSetupWindows.kt
+++ citizen-card-reader-lib/src/main/kotlin/com/rmpt/citizencard/reader/peidlib/setup/PteidSetupWindows.kt
@@ -16,8 +16,15 @@ class PteidSetupWindows : PteidSetup() {
     override fun run(): String {
         val tempDir = createTempDir()
         val nativeLibTmpFile = createEmptyFile(tempDir, libFile, true)
-        copyResourceFileToSystemFile("$rootDir$libFile", nativeLibTmpFile)
+        val archSubDir = if(isX64()) "x64/" else "x86/"
+        copyResourceFileToSystemFile("$rootDir$archSubDir$libFile", nativeLibTmpFile)
         log.info { "Main lib '$libFile' copied to '${nativeLibTmpFile.absolutePath}'" }
         return nativeLibTmpFile.absolutePath
+    }
+
+    private fun isX64(): Boolean {
+        val arch = System.getenv("PROCESSOR_ARCHITECTURE")
+        val arch64 = System.getenv("PROCESSOR_ARCHITEW6432")
+        return (arch != null && arch.endsWith("64")) || (arch64 != null && arch64.endsWith("64"))
     }
 }
